b7924406c84f355aa8dd37b3d9ea8f95d801d327
henrydog7 <balog7lucas@gmail.com>
2026-09-24 15:59:29 -0400

    post lab done

--- Lab1/interpolate.py
+++ Lab1/interpolate.py
@@ -5,6 +5,8 @@ import torch
 import torchvision.transforms as transforms
 from torchvision.datasets import FashionMNIST
 
+import numpy as np
+
 from model import autoencoderMLP4Layer
 
 DATA_DIR = './data'   # change if using Colab
@@ -19,11 +21,13 @@ def main():
     argParser = argparse.ArgumentParser()
     argParser.add_argument('-s', metavar='state', type=str, help='parameter file (.pth)')
     argParser.add_argument('-z', metavar='bottleneck size', type=int, help='int [8]')
+    argParser.add_argument('-n', metavar='n', type=int, help='int [8]')
 
     args = argParser.parse_args()
 
     save_file = args.s
     bottleneck_size = args.z
+    n = args.n
 
     device = 'cpu'
     if torch.cuda.is_available():
@@ -54,31 +58,45 @@ def main():
         if idx >= len(test_set):
             print('index out of range')
             continue
-
+        
+        idx2 = idx + 10 % len(test_set)
+        
         label = test_set.targets[idx].item()
         print('label =', label, '(' + CLASSES[label] + ')')
 
         img = test_set.data[idx].type(torch.float32) / 255.0  #normalize
         img = img.view(1, N_input).to(device=device) #flatten
+        img2 = test_set.data[idx2].type(torch.float32) / 255.0  #normalize
+        img2 = img2.view(1, N_input).to(device=device) #flatten
         
-        encoder_output = model.encoder(img)
-        model_output = model.decoder(encoder_output)
+        z1 = model.encode(img)
+        z2 = model.encode(img2)
+        
+        a = torch.linspace(0, 1, n).view(n, 1).to(device=device)
+        
+        z = (1-a)*z1 + a*z2
+        
+        interpolated = model.decode(z)
+        
 
         with torch.no_grad():
             output = model(img)
 
         img = img.view(28, 28).cpu()
         output = output.view(28, 28).cpu()
+        interpolated = interpolated.view(n, 28, 28).detach().cpu()
 
         f = plt.figure()
-        f.add_subplot(1, 2, 1)
+        f.add_subplot(1, n-1, 1)
         plt.imshow(img, cmap='gray')
         plt.title('input: ' + CLASSES[label])
-        f.add_subplot(1, 2, 2)
-        plt.imshow(output, cmap='gray')
-        plt.title('reconstruction')
+        for i in range(n-2):
+            f.add_subplot(1, n-1, i+2)
+            plt.imshow(interpolated[i], cmap='gray')
+            plt.title('interpolated')
+        
         plt.show()
-        plt.savefig("testPlt.png")
+        plt.savefig("testPltInterploate.png")
 
 
 if __name__ == '__main__':
